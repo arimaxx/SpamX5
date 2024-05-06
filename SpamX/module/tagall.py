cb4b4abb48d753d152080d5e529279f6a3c4253d
arimaxx <155280132+arimaxx@users.noreply.github.com>
2024-05-06 06:32:13 +0530

    Create tagall.py

---
+++ SpamX/module/tagall.py
@@ -0,0 +1,84 @@
+
+from asyncio import sleep
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+spam_chats = []
+
+def add_command_help(module_name, commands):
+    if module_name in CMD_HELP.keys():
+        command_dict = CMD_HELP[module_name]
+    else:
+        command_dict = {}
+
+    for x in commands:
+        for y in x:
+            if y is not x:
+                command_dict[x[0]] = x[1]
+
+    CMD_HELP[module_name] = command_dict
+def get_arg(message: Message):
+    msg = message.text
+    msg = msg.replace(" ", "", 1) if msg[1] == " " else msg
+    split = msg[1:].replace("\n", " \n").split(" ")
+    if " ".join(split[1:]).strip() == "":
+        return ""
+    return " ".join(split[1:])
+
+@Client.on_message(filters.command("tagall", ".") & filters.me)
+async def mentionall(client: Client, message: Message):
+    chat_id = message.chat.id
+    direp = message.reply_to_message
+    args = get_arg(message)
+    if not direp and not args:
+        return await message.edit("**Send me a message or reply to a message!**")
+    await message.delete()
+    spam_chats.append(chat_id)
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.get_chat_members(chat_id):
+        if not chat_id in spam_chats:
+            break
+        usrnum += 1
+        usrtxt += f"[{usr.user.first_name}](tg://user?id={usr.user.id}), "
+        if usrnum == 5:
+            if args:
+                txt = f"{args}\n\n{usrtxt}"
+                await client.send_message(chat_id, txt)
+            elif direp:
+                await direp.reply(usrtxt)
+            await sleep(2)
+            usrnum = 0
+            usrtxt = ""
+    try:
+        spam_chats.remove(chat_id)
+    except:
+        pass
+
+
+@Client.on_message(filters.command("cancel", ".") & filters.me)
+async def cancel_spam(client: Client, message: Message):
+    if not message.chat.id in spam_chats:
+        return await message.edit("**It seems there is no tagall here.**")
+    else:
+        try:
+            spam_chats.remove(message.chat.id)
+        except:
+            pass
+        return await message.edit("**Cancelled.**")
+
+
+add_command_help(
+    "tagall",
+    [
+        [
+            "tagall [text/reply ke chat]",
+            "Tag all the members one by one",
+        ],
+        [
+            "cancel",
+            f"to stop .tagall",
+        ],
+    ],
+)
